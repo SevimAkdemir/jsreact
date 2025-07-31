160917225ba13ff2f15016f392481fae8e78080a
SevimAkdemir <akdemir.seviim@gmail.com>
2025-07-31 08:48:56 +0300

    Card

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+  return(
+    <div className="card">
+        <img className= "photo"src="https://via.placeholder.com/150"></img>
+        <h3>Sevim</h3>
+        <p>I make crochet plushies and clothes.</p>
+    </div>
+  )
+}
+
+export default Card
