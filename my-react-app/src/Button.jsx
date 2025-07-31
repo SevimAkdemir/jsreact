e8e25ceef41bbf72b42e4f71f0b6fc012fbb4e53
SevimAkdemir <akdemir.seviim@gmail.com>
2025-07-31 12:48:15 +0300

    inline

---
+++ my-react-app/src/Button.jsx
@@ -0,0 +1,16 @@
+function Button(){
+
+  const styles = {
+    backgroundColor: "violet",
+    color: "white",
+    padding: "10px 20px",
+    border:"none",
+    cursor: "pointer,"
+  }
+
+  return(
+    <button style={styles}>Click</button>
+  )
+}
+
+export default Button
